e75660895b096b6dde8962582937def3d9fb20a3
daminglu <daminglu@yahoo.com>
2018-10-03 16:19:18 -0700

    play audio is working

--- vue_news_reader/scripts/components.js
+++ vue_news_reader/scripts/components.js
@@ -3,13 +3,13 @@
  */
 
 // register the grid component
-Vue.component('page-head', {
-  template: '#page-head'
-})
-
-Vue.component('grid', {
-  template: '#grid',
-  props: ['colors']
-
-})
+//Vue.component('page-head', {
+//  template: '#page-head'
+//})
+//
+//Vue.component('grid', {
+//  template: '#grid',
+//  props: ['colors']
+//
+//})
 
