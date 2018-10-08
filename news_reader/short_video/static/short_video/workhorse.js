e18d08669d4fac0a39de8064cba17c95c9405a1d
daminglu <daminglu@yahoo.com>
2018-10-08 16:04:34 -0700

    bind click on video to other events, TODO: render comments based on timeline

--- news_reader/short_video/static/short_video/workhorse.js
+++ news_reader/short_video/static/short_video/workhorse.js
@@ -1,1 +1,23 @@
 console.log('haha');
+
+//function playVideo(this) {
+//  console.log('playVideo');
+//  this.play();
+//};
+
+$(document).ready(function(){
+    console.log('doc ready');
+    $("#video_cover").click(function() {
+        console.log('play!');
+        var video = $("#video_element").get(0);
+        video.play();
+
+        $(this).css("visibility", "hidden");
+        return false;
+    });
+
+    $("#video_element").bind("pause ended", function() {
+        console.log('pause ended');
+        $("#video_cover").css("visibility", "visible");
+    });
+});
